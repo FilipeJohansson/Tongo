c689d2dd7d961e569d539d15d9c552899a78714e
DeltaRig <hitomikaleiko@gmail.com>
2021-01-07 17:41:56 -0300

    stop funcionando

--- commands/music/stop.js
+++ commands/music/stop.js
@@ -1,10 +1,10 @@
 module.exports = {
-	name: 'next',
-    description: 'Passa para a próxima música.',
+	name: 'stop',
+    description: 'Para de tocar música.',
     args: false,
     guildOnly: true,
     permissions: 'ADMINISTRATOR',
-	execute(message, args) {
+	async execute(message, args) {
         const channelVoice = message.member.voice;
         if (!channelVoice) 
             return message.channel.send('Você precisa estar em um canal de voz para usar esse comando');
@@ -12,12 +12,10 @@ module.exports = {
 		const serverQueue = message.client.queue.get(message.guild.id);
         if (!serverQueue) 
             return message.channel.send('Não há nenhuma música tocando.');
-
-        if (!serverQueue.playing)            
-			return message.channel.send('Você não pode passar uma música pausada.');
         
+		serverQueue.songs = [];
         serverQueue.connection.dispatcher.end();
         
-        return message.channel.send('Música passada adiante.');
+        return message.channel.send('Parando de tocar música.');
     }
 }
