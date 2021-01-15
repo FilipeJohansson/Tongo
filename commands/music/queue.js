fffb5181eb6539d8f487cfc037ab9e678c2ccbc4
FilipeJohasson <filipejohansson7@gmail.com>
2021-01-15 11:19:19 -0300

    music queue: validando se possui mais de uma música na lista para colocar a frase no singular ou no plural

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -5,7 +5,7 @@ module.exports = {
     description: 'Mostra a fila de músicas.',
     args: false,
     guildOnly: true,
-    aliases: ['fila', 'lista'],
+    aliases: ['fila', 'lista', 'listar', 'list'],
 	async execute(message, args) {
         const serverQueue = message.client.queue.get(message.guild.id);
 
@@ -18,8 +18,16 @@ module.exports = {
             .setColor([155, 31, 196])
             .setTitle("Fila de músicas")
             .setDescription(`__Tocando agora:__\n[${serverQueue.songs[0].title}](${serverQueue.songs[0].url})\n\n__Músicas na fila:__\n${serverQueue.songs.map(song => `[${i++}] - [${song.title}](${song.url})`).join('\n')}`)
-            .setFooter(`${serverQueue.songs.length} músicas na fila`, `https://media1.tenor.com/images/a51ea91ab28cef8aa211fdea29ae89d3/tenor.gif`)
             .setThumbnail("https://media1.tenor.com/images/a51ea91ab28cef8aa211fdea29ae89d3/tenor.gif");
+
+        var x = null;
+
+        if(i == 2)
+            x = 'música na fila';
+        else
+            x = 'músicas na fila';
+
+        embed.setFooter(`${serverQueue.songs.length} ${x}`, `https://media1.tenor.com/images/a51ea91ab28cef8aa211fdea29ae89d3/tenor.gif`)
         return message.channel.send(embed);
 
     }
